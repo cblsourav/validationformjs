6a7943eb89cab4d16bbe295ade44f1d4aad28861
cblsourav <cblsouravkumar@gmail.com>
2023-12-29 18:29:34 +0530

    date29/12/2023

---
+++ script.js
@@ -0,0 +1,38 @@
+// const data = {
+//     name: 'sourav',
+//     age: 29,
+//     class: 'BCA',
+// }
+
+// const hello = () => { return `${data.name}<br>${data.class}` };
+
+// document.write(hello());
+
+
+
+
+// const employee = {
+//     calTax() {
+//         console.log('tax rate is 10%')
+//     }
+// }
+
+// const karan = {
+//     salary: 5000,
+// }
+
+// karan.__proto__ = employee
+
+
+
+
+
+document.getElementById("demo").innerHTML =
+    x.toString() + "<br>" +
+    (123).toString() + "<br>" +
+    (100 + 23).toString();
+
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,32 @@
+
+
+// let element0 = document.getElementsByClassName("helo")
+
+// console.log(element0);
+
+let element8 = document.getElementsByClassName("helo").inneHTML=
+
+console.log(element8);
+
+
+
+
+
+
+
+
+
+
+let element = document.links
+
+console.log(element);
+
+
+let element2 = document.images
+console.log(element2);
+
+let element3 = document.forms
+console.log(element3);
+
+let element4 = document.fonts
+console.log(element4);
